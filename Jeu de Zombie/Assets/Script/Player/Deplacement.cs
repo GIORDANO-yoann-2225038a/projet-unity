4632a59fb074d89b6eebcfd76432eecc28c1d51a
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2024-12-28 03:44:55 +0100

    Animation Player

--- Jeu de Zombie/Assets/Script/Player/Deplacement.cs
+++ Jeu de Zombie/Assets/Script/Player/Deplacement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Deplacement : MonoBehaviour
 {
 //movement speed in units per second
-    public float movementSpeed = 5f;
+    public float movementSpeed = 0.2f;
     public bool recup = false;
     public int etages;
     public bool isRun= false;
@@ -13,42 +13,26 @@ public class Deplacement : MonoBehaviour
 
     void Start ()
     {
-        animations = GameObject.Find("Soldat").GetComponent<Animator>();
+        animations = GameObject.Find("Soldier").GetComponent<Animator>();
     }
 
     void Update()
     {
-        //get the Input from Vertical axis
+        // Récupérer les entrées des axes
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
-                // Si le joueur se déplace horizontalement ou verticalement
-        if (verticalInput > 0 )
-        {
-            animations.SetBool("IsRun", true);
-            animations.SetFloat("vertical",verticalInput);
-        }
-        
-        else if (verticalInput < 0 )
-        {
-            Debug.Log(verticalInput);
-            animations.SetBool("IsRun", true);
-            animations.SetFloat("vertical",verticalInput);
-        }
 
-          else if (Input.GetButton("Fire1"))
-        {
-            animations.SetBool("IsShoot",true);
-        }
-         else if (Input.GetButton("Zoom"))
-        {
-            animations.SetBool("IsPoint",true);
-        }
-        else
-        {
-            animations.SetBool("IsRun",false);
-            animations.SetBool("IsShoot",false);
-            animations.SetBool("IsPoint",false);
-        }
+        // Vérifier si le joueur se déplace
+        bool isMoving = verticalInput != 0 || horizontalInput != 0;
+        animations.SetBool("IsRun", isMoving);
+
+        // Mettre à jour les paramètres de l'Animator
+        animations.SetFloat("vertical", verticalInput);
+        animations.SetFloat("horizontal", horizontalInput);
+
+        // Gérer les autres actions
+        animations.SetBool("IsShoot", Input.GetButton("Fire1"));
+        animations.SetBool("IsPoint", Input.GetButton("Zoom"));
 
         //update the position
         transform.Translate(horizontalInput*movementSpeed * Time.deltaTime,0,verticalInput*movementSpeed * Time.deltaTime);
